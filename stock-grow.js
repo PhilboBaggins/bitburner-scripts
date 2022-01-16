eac0e22084087abf9987b8b0a3d134d6dee37715
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-17 00:52:54 +1100

    Formatting fixes

--- stock-grow.js
+++ stock-grow.js
@@ -2,7 +2,7 @@
 export async function main(ns) {
     const args = ns.flags([['help', false]]);
     const hostname = args._[0];
-    if(args.help || !hostname) {
+    if (args.help || !hostname) {
         ns.tprint('This script will "grow" a server for the purpose of increasing its company\'s stock price');
         ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME`);
         ns.tprint('Example:');
