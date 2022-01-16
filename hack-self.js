552d77c8d684b524ba6c9b1b5026295e5c902f78
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-17 00:50:02 +1100

    Slightly more intelligent hack-self.js

--- hack-self.js
+++ hack-self.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     const args = ns.flags([['help', false]]);
-    if(args.help) {
+    if (args.help) {
         ns.tprint('This script will generate money by hacking a target server.');
         ns.tprint(`USAGE: run ${ns.getScriptName()}`);
         ns.tprint('Example:');
@@ -9,11 +9,16 @@ export async function main(ns) {
         return;
     }
 
-	let hostname = ns.getHostname(); 
+    let hostname = ns.getHostname(); 
+
+    // TODO: Are these sensible values???
+    let thresholdMoney = ns.getServerMaxMoney(hostname) * 0.75;
+    let thresholdSecurity = ns.getServerMinSecurityLevel(hostname) + 5;
+
     while (true) {
-        if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname)) {
+        if (ns.getServerSecurityLevel(hostname) > thresholdSecurity) {
             await ns.weaken(hostname);
-        } else if (ns.getServerMoneyAvailable(hostname) < ns.getServerMaxMoney(hostname)) {
+        } else if (ns.getServerMoneyAvailable(hostname) < thresholdMoney) {
             await ns.grow(hostname);
         } else {
             await ns.hack(hostname);
