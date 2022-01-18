10eb73f8a3c72c27a070fab4b3b34b9e00d5da90
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-18 22:12:22 +1100

    Add 1001.js

--- wget-my-scripts.js
+++ wget-my-scripts.js
@@ -9,6 +9,7 @@ async function wgetScript(ns, baseURL, fileName) {
 
 /** @param {NS} ns **/
 export async function main(ns) {
+    await wgetScript(ns, PHILBO_BAGGINS_REPO_BASE_URL, '1001.js');
     await wgetScript(ns, PHILBO_BAGGINS_REPO_BASE_URL, 'auto-root.js');
     await wgetScript(ns, PHILBO_BAGGINS_REPO_BASE_URL, 'deploy-to-all.js');
     await wgetScript(ns, PHILBO_BAGGINS_REPO_BASE_URL, 'find-file.js');
