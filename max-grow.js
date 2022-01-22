c27f940f047260826eb1132674fbd8b464fd706c
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-22 17:56:32 +1100

    Delete max-grow.js, max-hack.js and max-weaken.js

--- max-grow.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    let childScriptArgs = ['just-grow.js'].concat(ns.args);
-    ns.exec('max-script.js', ns.getHostname(), 1, ...childScriptArgs);
-}
--- max-weaken.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    let childScriptArgs = ['just-weaken.js'].concat(ns.args);
-    ns.exec('max-script.js', ns.getHostname(), 1, ...childScriptArgs);
-}
