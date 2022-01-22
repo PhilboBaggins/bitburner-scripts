b7884d5d8f28edae8b42b7858b9bee36d6f3e551
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-23 00:57:35 +1100

    Fix start.js

--- start.js
+++ start.js
@@ -5,10 +5,16 @@ export async function main(ns) {
     ns.disableLog('sleep');
     ns.disableLog('exec');
 
-    // Helper function to call execAndWait with common args filled in
+    // Helper functions
     async function myExecAndWait(childScript, ...childScriptArgs) {
         printEverywhere(ns, `Running ${childScript} ${childScriptArgs}`);
         await execAndWait(ns, childScript, ns.getHostname(), 1, ...childScriptArgs);
+        await ns.sleep(1 * 1000);
+    }
+    async function myExec(childScript, ...childScriptArgs) {
+        printEverywhere(ns, `Running ${childScript} ${childScriptArgs}`);
+        ns.exec(childScript, ns.getHostname(), 1, ...childScriptArgs);
+        await ns.sleep(1 * 1000);
     }
 
     // Run the wget script twice because it will download itself and that may update the wget script and lead to new downloads on the second run
@@ -18,12 +24,12 @@ export async function main(ns) {
 
     await myExecAndWait('killall-everywhere.js');
 
-    await myExecAndWait('hacknet-auto-purchase.js');
+    await myExec('hacknet-auto-purchase.js');
 
-    await myExecAndWait('auto-root.js');
-    await myExecAndWait('deploy-to-all.js', 'hack-self.js');
-    
-    await myExecAndWait('stock-auto-buy.js');
-    await myExecAndWait('stock-auto-manipulate.js');
-    await myExecAndWait('stock-auto-sell.js');
+    await myExec('stock-auto-buy.js');
+    await myExec('stock-auto-manipulate.js');
+    await myExec('stock-auto-sell.js');
+
+    await myExec('auto-root.js');
+    await myExec('deploy-to-all.js', 'hack-self.js');
 }
