c8471b2b051ab6ac0e8fb8b84e3a832027030f6c
Phil <PhilboBaggins@users.noreply.github.com>
2022-01-22 17:04:02 +1100

    Improve stock-position.js print outs, include totals and profit percentage

--- stock-position.js
+++ stock-position.js
@@ -18,13 +18,22 @@ export async function main(ns) {
         return;
     }
 
+    var totalShares = 0;
+    var totalValue = 0;
+    var totalProfit = 0;
     const stocks = ns.stock.getSymbols().sort(function (a, b) { return stockPositionInDollars(ns, b) - stockPositionInDollars(ns, a); })
     for (const stock of stocks) {
         const [shares, avgPx, sharesShort, avgPxShort] = ns.stock.getPosition(stock);
         if (shares > 0) {
             const value = stockPositionInDollars(ns, stock);
             const estProfit = value - (shares * avgPx);
-            ns.tprint(`${stock.padStart(4)}: ${numberFormat(ns, shares)} shares totalling $${numberFormat(ns, value)} ($${numberFormat(ns, estProfit)} profit)`);
+            ns.tprint(`${stock.padStart(5)}: ${numberFormat(ns, shares)} shares valued at $${numberFormat(ns, value)} with a potential profit of $${numberFormat(ns, estProfit)} (${ns.nFormat(estProfit / value, '0%').padStart(4)})`);
+
+            totalShares += shares;
+            totalValue += value;
+            totalProfit += estProfit;
         }
     }
+
+    ns.tprint(`Total: ${numberFormat(ns, totalShares)} shares valued at $${numberFormat(ns, totalValue)} with a potential profit of $${numberFormat(ns, totalProfit)} (${ns.nFormat(totalProfit / totalValue, '0%').padStart(4)})`);
 }
