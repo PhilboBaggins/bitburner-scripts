34a16797f2671821cf036b82fee0e052b3e80dd0
Phil <PhilboBaggins@users.noreply.github.com>
2022-06-11 18:58:40 +1000

    list-servers.js: Add 'cash %' and fix 'ram used'

--- list-servers.js
+++ list-servers.js
@@ -25,17 +25,30 @@ export async function main(ns) {
         return ns.getServer(b).requiredHackingSkill - ns.getServer(a).requiredHackingSkill;
     });
 
-    let total    = { 'level': 0, 'admin': 0, 'backdoor': 0, 'cash': 0, 'maxRam': 0, 'ramUsed': 0 };
-    let hackable = { 'level': 0, 'admin': 0, 'backdoor': 0, 'cash': 0, 'maxRam': 0, 'ramUsed': 0 };
+    let total    = { 'level': 0, 'admin': 0, 'backdoor': 0, 'cash': 0, 'cashPercentage': 0, 'maxRam': 0, 'ramUsed': 0 };
+    let hackable = { 'level': 0, 'admin': 0, 'backdoor': 0, 'cash': 0, 'cashPercentage': 0, 'maxRam': 0, 'ramUsed': 0 };
     let numHackable = 0;
 
-    function printRow(name, level, admin, backdoor, cash, maxRam, ramUsed) {
+    function percentage(portion, max) {
+        if ((portion > 0) && (max > 0)) {
+            return Math.ceil(portion / max);
+        } else if ((portion == 0) && (max == 0)) {
+            return 1; // 100%
+        } else if (max > 0) {
+            return 0;
+        } else {
+            return NaN;
+        }
+    }
+
+    function printRow(name, level, admin, backdoor, cash, maxRam, ramUsed, maxCash) {
         table.printRow(ns,
             name,
             ns.nFormat(level, '0'),
             admin ? 'Yes' : '',
             backdoor ? 'Yes' : '',
             ns.nFormat(cash, '0.0a'),
+            ns.nFormat(percentage(cash, maxCash), '0%'),
             ns.nFormat(maxRam, '0.0a') + 'G',
             ns.nFormat(ramUsed, '0%'),
         );
@@ -48,6 +61,7 @@ export async function main(ns) {
             ns.nFormat(totalsObj['admin'],    '0.0a'),
             ns.nFormat(totalsObj['backdoor'], '0.0a'),
             ns.nFormat(totalsObj['cash'],     '0.0a'),
+            ns.nFormat(totalsObj['cashPercentage'],  '0%'),
             ns.nFormat(totalsObj['maxRam'],   '0.0a') + 'G',
             ns.nFormat(totalsObj['ramUsed'],  '0%'),
         );
@@ -60,16 +74,18 @@ export async function main(ns) {
             ns.nFormat(totalsObj['admin']    / num, '0%'),
             ns.nFormat(totalsObj['backdoor'] / num, '0%'),
             ns.nFormat(totalsObj['cash']     / num, '0.0a'),
+            ns.nFormat(totalsObj['cashPercentage']  / num, '0%'),
             ns.nFormat(totalsObj['maxRam']   / num, '0.0a') + 'G',
             ns.nFormat(totalsObj['ramUsed']  / num, '0%'),
         );
     }
 
-    function addToTotals(server, level, admin, backdoor, cash, maxRam, ramUsed) {
+    function addToTotals(server, level, admin, backdoor, cash, maxRam, ramUsed, maxCash) {
         total['level'] += level;
         total['admin'] += admin ? 1 : 0;
         total['backdoor'] += backdoor ? 1 : 0;
         total['cash'] += cash;
+        total['cashPercentage'] += percentage(cash, maxCash);
         total['maxRam'] += maxRam;
         total['ramUsed'] += ramUsed;
 
@@ -78,6 +94,7 @@ export async function main(ns) {
             hackable['admin'] += admin ? 1 : 0;
             hackable['backdoor'] += backdoor ? 1 : 0;
             hackable['cash'] += cash;
+            hackable['cashPercentage'] += percentage(cash, maxCash);
             hackable['maxRam'] += maxRam;
             hackable['ramUsed'] += ramUsed;
             numHackable++;
@@ -87,8 +104,8 @@ export async function main(ns) {
     table.printHeader(ns);
     for (const server of servers) {
         const host = ns.getServer(server);
-        printRow(   server, host.requiredHackingSkill, host.hasAdminRights, host.backdoorInstalled, host.moneyAvailable, host.maxRam, Math.ceil(host.ramUsed / host.maxRam));
-        addToTotals(server, host.requiredHackingSkill, host.hasAdminRights, host.backdoorInstalled, host.moneyAvailable, host.maxRam, Math.ceil(host.ramUsed / host.maxRam));
+        printRow(   server, host.requiredHackingSkill, host.hasAdminRights, host.backdoorInstalled, host.moneyAvailable, host.maxRam, percentage(host.ramUsed, host.maxRam), host.moneyMax);
+        addToTotals(server, host.requiredHackingSkill, host.hasAdminRights, host.backdoorInstalled, host.moneyAvailable, host.maxRam, percentage(host.ramUsed, host.maxRam), host.moneyMax);
     };
     table.printHeader(ns);
     printRowTotals('Total:', total);
