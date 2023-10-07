0ac54f8f6f407c907b242d3f7b7b210f7ef9d88f
Phil <PhilboBaggins@users.noreply.github.com>
2023-10-07 18:48:46 +1100

    Limit percentage of RAM used in stock-grow-continuous.js

--- stock-grow-continuous.js
+++ stock-grow-continuous.js
@@ -4,11 +4,21 @@ import { threadsPossible } from 'common.js'
 export async function main(ns) {
     const args = ns.flags([['help', false]]);
     const hostname = args._[0];
-    if (args.help || !hostname) {
+    const percentageRAM = parseFloat((args._[1] + '').replace('%', ''));
+    if (args.help || !hostname || !percentageRAM) {
         ns.tprint('This script will "grow" a server for the purpose of increasing its company\'s stock price');
-        ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME`);
+        ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME PERCENTAGE_OF_RAM_TO_USE`);
         ns.tprint('Example:');
-        ns.tprint(`> run ${ns.getScriptName()} n00dles`);
+        ns.tprint(`> run ${ns.getScriptName()} n00dles 50`);
+        return;
+    }
+
+    // Check arguements
+    if (percentageRAM > 100.0) {
+        ns.tprint(`Percentage or RAM to use (${percentageRAM}) over 100%`);
+        return;
+    } else if (percentageRAM < 0.0) {
+        ns.tprint(`Percentage or RAM to use (${percentageRAM}) less than 0%`);
         return;
     }
 
@@ -19,8 +29,9 @@ export async function main(ns) {
     const desiredIntervalMS = 10 * 1000; // 10 seconds
     const growTime = ns.getGrowTime(hostname);
     const numScripts = Math.floor(growTime / desiredIntervalMS);
-    const numThreads = threadsPossible(ns, childScript,  ns.getHostname());
+    const numThreads = Math.floor(threadsPossible(ns, childScript,  ns.getHostname()) / 100.0 * percentageRAM);
     const numThreadsPerScript = Math.floor(Math.max(1, numThreads / numScripts));
+
     ns.tprint(`Launching script ${childScript} against ${hostname} (grow time: ${ns.tFormat(growTime)}) with ${ns.nFormat(numThreads, '0.000a')} threads spread over ${ns.nFormat(numScripts, 'a')} scripts (${ns.nFormat(numThreadsPerScript, 'a')} per script)`);
     for (let i = 0; i < numScripts; i++) {
         ns.exec(childScript, ns.getHostname(), numThreadsPerScript, hostname, i); // The `i` arg will be ignored by `stock-grow.js`, just added so that I can launch multiple `stock-grow.js` scripts
