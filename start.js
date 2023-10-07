b7a5e989db1f984c57ae6626d2d4e7d451b3b78e
Phil <PhilboBaggins@users.noreply.github.com>
2023-10-07 18:37:59 +1100

    Control number of threads in start.js

--- start.js
+++ start.js
@@ -6,30 +6,30 @@ export async function main(ns) {
     ns.disableLog('exec');
 
     // Helper functions
-    async function myExecAndWait(childScript, ...childScriptArgs) {
+    async function myExecAndWait(childScript, numThreads, ...childScriptArgs) {
         printEverywhere(ns, `Running ${childScript} ${childScriptArgs}`);
-        await execAndWait(ns, childScript, ns.getHostname(), 1, ...childScriptArgs);
+        await execAndWait(ns, childScript, ns.getHostname(), numThreads, ...childScriptArgs);
         await ns.sleep(1 * 1000);
     }
-    async function myExec(childScript, ...childScriptArgs) {
+    async function myExec(childScript, numThreads, ...childScriptArgs) {
         printEverywhere(ns, `Running ${childScript} ${childScriptArgs}`);
-        ns.exec(childScript, ns.getHostname(), 1, ...childScriptArgs);
+        ns.exec(childScript, ns.getHostname(), numThreads, ...childScriptArgs);
         await ns.sleep(1 * 1000);
     }
 
     // Run the wget script twice because it will download itself and that may update the wget script and lead to new downloads on the second run
     // Recently downloaded files seem to be cached and will download quickly, so running the wget script twice shouldn't cause much delay
-    await myExecAndWait('wget-my-scripts.js');
-    await myExecAndWait('wget-my-scripts.js');
+    //await myExecAndWait('wget-my-scripts.js', 1);
+    //await myExecAndWait('wget-my-scripts.js', 1);
 
-    await myExecAndWait('killall-everywhere.js');
+    await myExecAndWait('killall-everywhere.js', 1);
 
-    await myExec('hacknet-auto-purchase.js');
+    await myExec('hacknet-auto-purchase.js', 1);
 
-    await myExec('stock-auto-buy.js');
-    await myExec('stock-auto-manipulate.js');
-    await myExec('stock-auto-sell.js');
+    await myExec('stock-auto-buy.js', 1);
+    await myExec('stock-auto-manipulate.js', 1);
+    await myExec('stock-auto-sell.js', 1);
 
-    await myExec('auto-root.js');
-    await myExec('deploy-to-all.js', 'hack-self.js');
+    await myExec('auto-root.js', 1);
+    await myExec('deploy-to-all.js', 1, 'hack-self.js');
 }
